eb682d7fb3c5596ca1490f699d7e1967d1d89c2a
“Sonusaggu” <sonusaggu92@gmail.com>
2025-07-19 20:06:29 -0600

    more enh

--- app.py
+++ app.py
@@ -63,4 +63,5 @@ class TSXScraper:
         return response.json().get("data") if response.text != "null" else None
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
